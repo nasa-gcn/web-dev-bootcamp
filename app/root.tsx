714ed0183bd13f8938872945d2f79d5da2012f97
Courey Elliott <coureyelliott@gmail.com>
2023-08-11 17:14:15 -0400

    adding a notes index and details page with CRUD functionality

--- app/root.tsx
+++ app/root.tsx
@@ -55,6 +55,9 @@ export default function App() {
         <PrimaryNav
           className="margin-top-1"
           items={[
+            <NavLink className="" to="/notes" key="/notes">
+              Notes
+            </NavLink>,
             <NavLink className="" to="/data" key="/data">
               Data
             </NavLink>,
